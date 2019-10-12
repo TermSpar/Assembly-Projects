3fd4398fcd0ca678ea7fd6edf24b01e8e5685c9b
Ben Bollinger <benbollinger271@yahoo.com>
2019-10-11 21:08:00 -0500

    added more

---
+++ RandomXs.asm
@@ -0,0 +1,64 @@
+INCLUDE Irvine32.inc
+
+.data
+
+str1 BYTE "X",0
+
+yPos BYTE ?
+xPos BYTE ?
+
+coordListX BYTE 50 DUP(?)
+coordListY BYTE 50 DUP(?)
+
+.code
+main PROC
+
+	; loop 50 times
+	mov ecx,50
+
+	call Randomize
+	; draw x loop:
+	L1:
+		; generate random row position:
+		mov eax,10
+		call RandomRange
+		mov yPos,al
+
+		; log yPos:
+		mov [coordListX+ecx],al
+
+		; clear eax:
+		mov eax,0
+
+		; generate random col position:
+		mov eax,100
+		call RandomRange
+		mov xPos,al
+
+		; log xPos:
+		mov [coordListY+ecx],al
+
+		; draw the X:
+		call DrawX
+		
+		loop L1
+	exit
+main ENDP
+
+; input an integer
+DrawX PROC
+	call Locate
+	mov  edx,OFFSET str1
+	call WriteString
+	ret
+DrawX ENDP
+
+; locate the cursor
+Locate PROC
+	mov  dh,yPos
+	mov  dl,xPos
+	call Gotoxy
+	ret
+Locate ENDP
+
+END main
---
+++ SwitchElements.asm
@@ -0,0 +1,56 @@
+INCLUDE Irvine32.inc
+
+.data
+
+numList BYTE 1,2,3,4,5,6,7,8,9
+ArraySize = ($ - numList)
+sizeMinus DWORD ?
+
+.code
+main PROC
+	; get size-1
+	mov ecx,ArraySize
+	dec ecx
+	mov sizeMinus,ecx
+
+	; set init index to 0:
+	mov ecx,0
+	L1:
+		; check if end of loop:
+		cmp ecx,sizeMinus
+		je exitL1
+
+		; get current iteration plus one in ebx:
+		mov ebx,ecx
+		inc ebx
+		
+		; make the switch:
+		mov dl,[numList+ebx]
+		mov dh,[numList+ecx]
+		
+		mov [numList+ebx],dh
+		mov [numList+ecx],dl
+
+		inc ecx
+	jmp L1
+
+	exitL1:
+
+	; display new array:
+	mov eax,0
+	mov ecx,0
+	L2:
+		; check if end of loop:
+		cmp ecx,ArraySize
+		je quit
+
+		; display current iteration:
+		mov al,[numList+ecx]
+		call WriteInt
+		inc ecx
+	jmp L2
+
+	quit:
+	exit
+main ENDP
+END main
